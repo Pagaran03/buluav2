3942e38182b0a3d51061c8d57e31c37ee5a534b7
amabee <cyberkiller.ck@gmail.com>
2024-04-30 09:53:09 +0800

    updated with time sorting

--- staff/consultation/consultation_content.php
+++ staff/consultation/consultation_content.php
@@ -1,22 +1,24 @@
 <?php
-// Include your database configuration file
+
 include_once ('../../config.php');
 
 $date = date('Y-m-d');
 
-$sql = "SELECT *,consultations.id as id,CONCAT(patients.last_name,',',patients.first_name) AS full_name
-FROM consultations
-JOIN patients ON consultations.patient_id = patients.id
-WHERE consultations.is_active = 0 AND consultations.is_deleted=0 AND checkup_date = $date";
+if (isset($_GET['date'])) {
+    $selected_date = date('Y-m-d', strtotime($_GET['date']));
+    $date = $selected_date;
+}
 
-
+$sql = "SELECT *, consultations.id AS id, CONCAT(patients.last_name, ',', patients.first_name) AS full_name
+        FROM consultations
+        JOIN patients ON consultations.patient_id = patients.id
+        WHERE consultations.is_active = 0 AND consultations.is_deleted = 0 AND checkup_date = '$date'";
 
 $result = $conn->query($sql);
 
 if ($result === false) {
     die("Query failed: " . $conn->error);
 }
-
 ?>
 
 <div class="container-fluid">
@@ -39,9 +41,11 @@ if ($result === false) {
         </button>
     </a> -->
     <div style="text-align: left; float: left; margin-left: 10px;">
-        <input type="text" class="form-control" id="datepicker" placeholder="Select Date">
+        <form action="" method="GET">
+            <input type="text" class="form-control" id="datepicker" name="date" placeholder="Select Date"
+                onchange="this.form.submit()">
+        </form>
     </div>
-
 
     <br><br>
 
