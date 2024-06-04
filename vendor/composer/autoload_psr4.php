c67083bf641dc26a9cdc73464dc3f11ba8a80ac8
kashiki123 <joub.obrial.coc@phinmaed.com>
2024-06-04 00:12:49 -1200

    check consult name

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -20,6 +20,7 @@ return array(
     'Symfony\\Contracts\\Cache\\' => array($vendorDir . '/symfony/cache-contracts'),
     'Symfony\\Component\\VarExporter\\' => array($vendorDir . '/symfony/var-exporter'),
     'Symfony\\Component\\Validator\\' => array($vendorDir . '/symfony/validator'),
+    'Symfony\\Component\\TypeInfo\\' => array($vendorDir . '/symfony/type-info'),
     'Symfony\\Component\\String\\' => array($vendorDir . '/symfony/string'),
     'Symfony\\Component\\Serializer\\' => array($vendorDir . '/symfony/serializer'),
     'Symfony\\Component\\PropertyInfo\\' => array($vendorDir . '/symfony/property-info'),
